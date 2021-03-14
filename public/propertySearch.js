a2ddde8749054f7a4218d813c67582a2d01f0cae
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-03-14 16:01:35 -0600

    Added in the basic API

---
+++ public/propertySearch.js
@@ -0,0 +1,13 @@
+var url = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=<" + zwsid + ">&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA";
+var zwsid = "X1-ZWz1i9miug76kr_1mzvh"
+
+
+var request = new XMLHttpRequest()
+request.open('GET', url, true)
+
+request.onload = function () {
+  // Begin accessing JSON data here
+}
+
+// Send request
+request.send()
