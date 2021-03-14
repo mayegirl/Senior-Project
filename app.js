c11e0eb28d4a628a6f24b69d2d0460428c117f10
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-03-14 15:01:43 -0600

    init

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const PORT = process.env.PORT || 5000
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('view engine', 'ejs')
+    .set('views', path.join(__dirname, 'views'))
+    .get('/', function (req, res) {res.sendFile(__dirname + '/public/main.html')})
+    .get('/getRate', function (req, res) {res.render('getRate')})
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
