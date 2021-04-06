a10ec110cc4f73dac754bfe5c492b03616c9f1c5
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-04-06 17:28:54 -0600

    testing

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 const express = require('express')
 const PORT = process.env.PORT || 3306
 const path = require('path')
-var usersRouter = require('routes/users.js');
+var usersRouter = require('routes\users.js');
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('view engine', 'ejs')
     .set('views', path.join(__dirname, 'views'))
     .get('/', function (req, res) {res.sendFile(__dirname + '/public/main.html')})
-    .get('/user-list', function (req, res) {res.render('users')})
+    .get('/user-list', function (req, res) {res.render(usersRouter)})
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
