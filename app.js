d6975b63d18934cd3e74260d12b39fdfddb8b461
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-04-06 20:31:42 -0600

    switching the main page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('view engine', 'ejs')
     .set('views', path.join(__dirname, 'views'))
-    .get('/', function (req, res) {res.sendFile(__dirname + '/public/main.html')})
+    .get('/', function (req, res) {res.sendFile(__dirname + 'db_connection.js')})
     .get('/user-list', function (req, res) {res.render(usersRouter)})
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
