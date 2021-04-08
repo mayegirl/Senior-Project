b73cae59488653c63ee831ed5bad493a51a9c777
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-04-07 22:25:01 -0600

    Finally displaying the dB

--- public/db_connection.js
+++ public/db_connection.js
@@ -1,66 +1,25 @@
-let template_table_header = {
-    "<>" : "tr", "html": [
-    {"<>": "th", "html": "Title"},
-    {"<>": "th", "html": "City ID"},
-    {"<>": "th", "html": "Estate Style"},
-    {"<>": "th", "html": "Square Footage"},
-    {"<>": "th", "html": "Bedrooms"},
-    {"<>": "th", "html": "Bathrooms"},
-    {"<>": "th", "html": "Garages Spaces"},
-    {"<>": "th", "html": "Total Parking Spaces"},
-    {"<>": "th", "html": "Washer/Dryer"},
-    {"<>": "th", "html": "Fridge/Freezer"},
-    {"<>": "th", "html": "Oven/Stove"},
-    {"<>": "th", "html": "Study"},
-    {"<>": "th", "html": "Attic Space"},
-    {"<>": "th", "html": "Description"}
-    ]
-}
-
-let template_table_body = {
-    "<>" : "tr", "html": [
-    {"<>": "td", "html": "${Title}"},
-    {"<>": "td", "html": "${City}"},
-    {"<>": "td", "html": "${Estate Style}"},
-    {"<>": "td", "html": "${Square Footage}"},
-    {"<>": "td", "html": "${Bedrooms}"},
-    {"<>": "td", "html": "${Bathrooms}"},
-    {"<>": "td", "html": "${Garage Spaces}"},
-    {"<>": "td", "html": "${Total Parking Spaces}"},
-    {"<>": "td", "html": "${Washer/Dryer}"},
-    {"<>": "td", "html": "${Fridge/Freezer}"},
-    {"<>": "td", "html": "${Oven/Stove}"},
-    {"<>": "td", "html": "${Study}"},
-    {"<>": "th", "html": "${Attic Space}"},
-    {"<>": "td", "html": "${Description}"}
-    ]
-}
-
-
 function writeHTMLfromJSON(){
-    let data = '../private/database.json';
-
     var txt = "";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var myJson = JSON.parse(this.responseText);
+        myJson = JSON.parse(this.responseText);
         txt += "<table border='1'>"
-        txt += "<tr><th>'Title'</th><th>'City'</th><th>'Estate Style'</th><th>'Square Footage'</th><th>'Bedrooms'</th><th>'Bathrooms'</th><th>'Garage Spaces'</th><th>'Total Parking Space'</th><th>'Washer/Dryer'</th><th>'Fridge/Freezer'</th><th>'Oven/Stove'</th><th>'Study'</th><th>'Attic'</th><th>'Description'</th></tr>";
-        myJson.forEach(function (x) {
-            txt += "<tr><td>" + x.Title + "</td> <td>" + x.City + "</td> <td>" + x
-                                .Estate_Style +"</td> <td>" + x.Square_Footage + "</td> <td>" + x
-                                .Bedrooms +"</td> <td>" + x.Bathrooms + "</td> <td>" + x
-                                .Garage_Spaces + "</td> <td>" + x.Total_Parking_Spaces + "</td> <td>" + x
-                                .Washer_Dryer +"</td> <td>" + x.Fridge_Freezer + "</td> <td>" + x
-                                .Oven_Stove + "</td> <td>" + x.Study + "</td> <td>" + x
-                                .Attic_Space + "</td> <td>" + x.Description + "</td></tr>";
-                        });
+        txt += "<tr><th>Title</th><th>City</th><th>Estate Style</th><th>Square Footage</th><th>Bedrooms</th><th>Bathrooms</th><th>Garage Spaces</th><th>Total Parking Space</th><th>Washer/Dryer</th><th>Fridge/Freezer</th><th>Oven/Stove</th><th>Study</th><th>Attic</th><th>Description</th></tr>";
+        for (i in myJson.properties) {
+            txt += "<tr><td>" + myJson.properties[i].Title + "</td> <td>" + myJson.properties[i].City + "</td> <td>" + myJson
+                                .properties[i].Estate_Style +"</td> <td>" + myJson.properties[i].Square_Footage + "</td> <td>" + myJson
+                                .properties[i].Bedrooms +"</td> <td>" + myJson.properties[i].Bathrooms + "</td> <td>" + myJson
+                                .properties[i].Garage_Spaces + "</td> <td>" + myJson.properties[i].Total_Parking_Spaces + "</td> <td>" + myJson
+                                .properties[i].Washer_Dryer +"</td> <td>" + myJson.properties[i].Fridge_Freezer + "</td> <td>" + myJson
+                                .properties[i].Oven_Stove + "</td> <td>" + myJson.properties[i].Study + "</td> <td>" + myJson
+                                .properties[i].Attic_Space + "</td> <td>" + myJson.properties[i].Description + "</td></tr>";
+                        };
                         txt += "</table>"
-        document.getElementById("display").innerHTML = this.responseText;
+        document.getElementById("display").innerHTML = txt;
     }
     };
-    xhttp.open("GET", data, true);
+    xhttp.open("GET", '../private/database.json', true);
     xhttp.send();
 
 }
